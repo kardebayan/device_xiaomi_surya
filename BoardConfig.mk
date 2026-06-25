a49d8fad4d5a2f8ff377cd4accc027435df4abeb
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:09:48 -0400

    surya: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,6 +72,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 TARGET_KERNEL_CONFIG := surya_defconfig
+TARGET_KERNEL_CLANG_VERSION := r563880c
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
